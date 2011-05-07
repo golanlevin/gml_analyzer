1403d0e21c4e3edcde62a6478dabe003672881a9
Max Hawkins <maxhawkins@gmail.com>
2011-05-07 18:09:22 -0400

    Added watchr for automatic testing

--- watchr.rb
+++ watchr.rb
@@ -3,8 +3,13 @@ def run_test(testfile)
   system("nosetests #{testfile}")
 end
 
+def run_all_tests
+  system("nosetests")
+end
+
 watch('gml_analyzer/([^\/]+)\.py') do |f|
-  run_test("gml_analyzer/test/test_#{f[1]}.py")
+  # run_test("gml_analyzer/test/test_#{f[1]}.py")
+  run_all_tests
 end
 
 watch('gml_analyzer/test/.*\.py') do |f|
