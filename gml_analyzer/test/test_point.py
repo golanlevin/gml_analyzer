d22bb54fe43cccf1505ae59e930b12e617b6750a
Max Hawkins <maxhawkins@gmail.com>
2011-05-11 17:44:00 -0400

    Add tests for joint_angle

--- gml_analyzer/test/test_point.py
+++ gml_analyzer/test/test_point.py
@@ -1,8 +1,72 @@
 import unittest
-from gml_analyzer.point import Point, PointXYT
+
 from numpy import all, equal
+from math import acos, pi
+
+from gml_analyzer.point import Point, PointXYT
 
 class PointTests(unittest.TestCase):
+  
+  def assertEqualRounded(self, a, b, **kwargs):
+    ACCURACY = 6
+    self.assertEqual( round(a, ACCURACY), round(b, ACCURACY), **kwargs)
+  
+  def test_up_right(self):
+    joint = (Point((0,0)), Point((0,1)), Point((1,1)))
+    self.assertEqualRounded( Point.joint_angle(*joint), pi/2 )
+  
+  def test_up_left(self):
+    joint = (Point((0,0)), Point((0,1)), Point((-1,1)))
+    self.assertEqualRounded( Point.joint_angle(*joint), -pi/2 )
+  
+  def test_left_up(self):
+    joint = (Point((0,0)), Point((-1,0)), Point((-1,1)))
+    self.assertEqualRounded( Point.joint_angle(*joint), pi/2 )
+  
+  def test_left_down(self):
+    joint = (Point((0,0)), Point((-1,0)), Point((-1,-1)))
+    self.assertEqualRounded( Point.joint_angle(*joint), -pi/2 )
+  
+  def test_down_left(self):
+    joint = (Point((0,0)), Point((0,-1)), Point((-1,-1)))
+    self.assertEqualRounded( Point.joint_angle(*joint), pi/2 )
+  
+  def test_down_right(self):
+    joint = (Point((0,0)), Point((0,-1)), Point((1,-1)))
+    self.assertEqualRounded( Point.joint_angle(*joint), -pi/2 )
+  
+  def test_right_up(self):
+    joint = (Point((0,0)), Point((1,0)), Point((1,1)))
+    self.assertEqualRounded( Point.joint_angle(*joint), -pi/2 )
+  
+  def test_right_down(self):
+    joint = (Point((0,0)), Point((1,0)), Point((1,-1)))
+    self.assertEqualRounded( Point.joint_angle(*joint), pi/2 )
+  
+  def test_colinear_joint_angle(self):
+    joint = (Point((1,1)), Point((2,1)), Point((3,1)))
+    self.assertEqualRounded( Point.joint_angle(*joint), 0 )
+  
+  def test_last_two_points_same_joint_angle(self):
+    joint = (Point((1,1)), Point((2,1)), Point((2,1)))
+    self.assertEqualRounded( Point.joint_angle(*joint), 0 )
+  
+  def test_first_two_points_same_joint_angle(self):
+    joint = (Point((1,1)), Point((1,1)), Point((2,1)))
+    self.assertEqualRounded( Point.joint_angle(*joint), 0 )
+  
+  def test_all_points_same_joint_angle(self):
+    joint = (Point((0,0)), Point((0,0)), Point((0,0)))
+    self.assertEqualRounded( Point.joint_angle(*joint), 0 )
+  
+  def test_points_folded_on_self_vertically_joint_angle(self):
+    joint = (Point((0,0)), Point((1,0)), Point((0,0)))
+    self.assertEqualRounded( Point.joint_angle(*joint), pi )
+  
+  def test_points_folded_on_self_horizontally_joint_angle(self):
+    joint = (Point((0,0)), Point((0,1)), Point((0,0)))
+    self.assertEqualRounded( Point.joint_angle(*joint), pi )
+  
   
   def test_x_getter(self):
     point = Point((1,2))
